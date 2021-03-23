2f04443efa7240cf476e43aa7321745cba96f9e3
Anders Genderskov Binder <heyanders@gmail.com>
2021-03-23 15:44:16 +0100

    Added Codegeneration logic in the form of the class AttendanceCodeGenerator.

---
+++ SkoleProtokolAPI/Controllers/CodeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SkoleProtokolAPI.Generator;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SkoleProtokolAPI.Controllers
+{
+    [Route("api/Code")]
+    [ApiController]
+    public class CodeController : ControllerBase
+    {
+        // GET: api/<CodeController>
+        [HttpGet]
+        public string Get()
+        {
+            string attendanceCode = AttendanceCodeGenerator.GenerateAttendanceCode();
+            return attendanceCode;
+        }
+
+        // GET api/<CodeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CodeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CodeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CodeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SkoleProtokolAPI/Generator/AttendanceCodeGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkoleProtokolAPI.Generator
+{
+    public static class AttendanceCodeGenerator
+    {
+
+
+
+
+
+
+        #region Methods
+        //Generates a six character code, the code will contain capital letters and numbers.
+        public static string GenerateAttendanceCode()
+        {
+            //int id = Guid.NewGuid().ToString().GetHashCode();
+
+            string attendanceCode = Guid.NewGuid().ToString("N").Substring(0, 6); //Should we avoid duplications? or can we fix it with timestamps?
+
+            return attendanceCode;
+        }
+
+        #endregion
+    }
+}
