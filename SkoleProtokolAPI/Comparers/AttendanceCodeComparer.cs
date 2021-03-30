a7df7700226f1987c66dd9b06e95b01369190cfb
Anders Genderskov Binder <heyanders@gmail.com>
2021-03-30 16:02:31 +0200

    Implemented a method for checking the validity of an attendanceCode.

---
+++ SkoleProtokolAPI/Comparers/AttendanceCodeComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SkoleProtokolAPI.ActiveTimer;
+
+namespace SkoleProtokolAPI.Comparers
+{
+    public static class AttendanceCodeComparer
+    {
+
+
+        #region Methods
+
+        public static bool IsCodeValid(ConcurrentQueue<ActiveAttendanceCode> activeAttendanceCodes, string codeToCompare)
+        {
+            bool codeIsValid = false;
+
+            if (activeAttendanceCodes.Count > 0)
+            {
+                foreach (ActiveAttendanceCode code in activeAttendanceCodes)
+                {
+                    if (String.Equals(code.AttendanceCode, codeToCompare))
+                    {
+                        codeIsValid = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                codeIsValid = true;
+            }
+
+            
+
+            return codeIsValid;
+        }
+
+        #endregion
+
+    }
+}
