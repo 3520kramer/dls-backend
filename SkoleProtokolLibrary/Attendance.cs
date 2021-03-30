a23bb8b423641112c32169d82d57b591711da5ad
Anders Genderskov Binder <heyanders@gmail.com>
2021-03-30 14:35:00 +0200

    Started working on connecting to a MongoDB, and added a class library for the entity models used for communication with the MongoDB.

---
+++ SkoleProtokolLibrary/Attendance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkoleProtokolLibrary
+{
+    public class Attendance
+    {
+        #region Properties
+
+        public DateTime Date { get; set; }
+
+        public string Class { get; set; }
+
+        public bool Attended { get; set; }
+
+
+        #endregion
+    }
+}
---
+++ SkoleProtokolLibrary/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SkoleProtokolLibrary
+{
+    public class User
+    {
+
+        #region Properties
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("firstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("classes")]
+        public List<string> Classes { get; set; }
+
+        [BsonElement("attendance")]
+        public List<Attendance> AttendanceLog { get; set; }
+
+        [BsonElement("role")]
+        public string Role { get; set; }
+
+        #endregion
+
+
+    }
+}
