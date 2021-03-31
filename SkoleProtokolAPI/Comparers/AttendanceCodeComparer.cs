226b7960fab35684e0f2973ac9aadbf3dc17f716
Anders Genderskov Binder <heyanders@gmail.com>
2021-03-31 13:25:54 +0200

    Implemented a method for comparing input code with the active codes.

--- SkoleProtokolAPI/Comparers/AttendanceCodeComparer.cs
+++ SkoleProtokolAPI/Comparers/AttendanceCodeComparer.cs
@@ -7,17 +7,28 @@ using SkoleProtokolAPI.ActiveTimer;
 
 namespace SkoleProtokolAPI.Comparers
 {
+    /// <summary>
+    /// A static class meant to contain all methods for comparing attendanceCodes
+    /// </summary>
     public static class AttendanceCodeComparer
     {
 
 
         #region Methods
-
-        public static bool IsCodeValid(ConcurrentQueue<ActiveAttendanceCode> activeAttendanceCodes, string codeToCompare)
+        /// <summary>
+        /// Checks all attendanceCode entries in a ConcurrentQueue for a match.
+        /// It is assumed that all attendanceCodes are unique.
+        /// If a match is found the method returns true,
+        /// if no match or the queue doesn't contain any codes then it return false
+        /// </summary>
+        /// <param name="activeAttendanceCodes">A ConcurrentQueue that contains activeAttendanceCodes</param>
+        /// <param name="codeToCompare">The attendanceCode that a match will be searched for</param>
+        /// <returns>bool</returns>
+        public static bool CheckCodeValidity(ConcurrentQueue<ActiveAttendanceCode> activeAttendanceCodes, string codeToCompare)
         {
             bool codeIsValid = false;
 
-            if (activeAttendanceCodes.Count > 0)
+            if (activeAttendanceCodes.Count > 0)//False is automatically returned if there are no entries
             {
                 foreach (ActiveAttendanceCode code in activeAttendanceCodes)
                 {
@@ -28,13 +39,7 @@ namespace SkoleProtokolAPI.Comparers
                     }
                 }
             }
-            else
-            {
-                codeIsValid = true;
-            }
-
             
-
             return codeIsValid;
         }
 
